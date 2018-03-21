70b374d9e77f93cc2c76c44fce788f4bab75ce3e
sbneto <samuel.m.b.neto@gmail.com>
2018-03-21 18:05:24 -0300

    fixing 2.7 compatibility

--- django_celery_tasks/middleware/globalrequestmiddleware.py
+++ django_celery_tasks/middleware/globalrequestmiddleware.py
@@ -11,14 +11,14 @@ class GlobalRequestMiddleware:
     @classmethod
     def get_current_request(cls):
         try:
-            return cls._threadmap[threading.get_ident()]
+            return cls._threadmap[threading.current_thread().ident]
         except KeyError:
             return None
 
     def __call__(self, request):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
-        self._threadmap[threading.get_ident()] = request
+        self._threadmap[threading.current_thread().ident] = request
         try:
 
             response = self.get_response(request)
@@ -29,6 +29,6 @@ class GlobalRequestMiddleware:
             return response
         finally:
             try:
-                del self._threadmap[threading.get_ident()]
+                del self._threadmap[threading.current_thread().ident]
             except KeyError:
                 pass
--- t/unit/test_task.py
+++ t/unit/test_task.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, unicode_literals
+from builtins import str
 
 import pytest
 
